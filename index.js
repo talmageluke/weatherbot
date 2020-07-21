dcfe3eed3cffbb4424316888f070ce7691dc32a6
talmage <talmagelucas@gmail.com>
2020-07-20 18:43:14 -0600

    added !5day, can now click thru multiple days

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { prefix, token, key } = require('./config.json');
+const { prefix, token } = require('./config.json');
 const fetch = require('node-fetch');
 const { send } = require('process');
 const { stringify } = require('querystring');
@@ -10,9 +10,8 @@ let channel = "";
 let withoutPrefix = "";
 let args = ""
 let authorID = ""
-
-
-
+var d = 0
+let currentMessage = ""
 
 client.once('ready', () => {
     console.log('Ready!');
@@ -33,9 +32,6 @@ client.on('message', message => {
     }
 });
 
-
-
-
 client.on('message', message => {
     authorID = message.authorID;
     if (message.content.startsWith(prefix + "weather") && authorID != "734621174541320202") {
@@ -55,6 +51,14 @@ client.on('message', message => {
         args = message.content.slice(prefix.length + 8).trim().split(' ');
         if (args[0].length === 5) {
             getForecast(channel, args);
+        }
+    }
+    if (message.content.startsWith(prefix + "5day") && authorID != "734621174541320202") {
+        channel = message.channel.id;
+        author = message.author.id
+        args = message.content.slice(prefix.length + 4).trim().split(' ');
+        if (args[0].length === 5) {
+            getDaily(channel, args, author);
         }
     }
 });
@@ -88,49 +92,138 @@ function getForecast(data) {
     fetch(`http://api.openweathermap.org/data/2.5/forecast?zip=${zipCode},US&appid=002cc3ba839c181a79f9d6443cd15640`)
         .then(function (response) { return response.json() })
         .then(function (data) {
-            console.log(data);
 
             tempF = Math.round(data.list[0].main.temp * 9 / 5 - 459.67);
             tempC = Math.round(data.list[0].main.temp - 273.15);
 
-
             var today = [Math.round(data.list[0].main.temp * 9 / 5 - 459.67)];
             var temps = [Math.round(data.list[0].main.temp * 9 / 5 - 459.67)];
-
-
             var dates = [data.list[0].dt_txt]
 
-
             for (i = 8; i < 40; i = i + 8) {
-
                 var nextDay = Math.round(data.list[i].main.temp * 9 / 5 - 459.67);
                 temps.push(nextDay);
                 dates.push(data.list[i].dt_txt)
-
-
                 tempF = Math.round(data.list[i].main.temp * 9 / 5 - 459.67);
                 tempC = Math.round(data.list[i].main.temp - 273.15);
-
-
-
             }
             client.channels.cache.get(channel).send({
                 embed: {
                     color: 3449993,
                     title: "5 Day Forecast:",
                     thumbnail: {
-                        url: 'https://i.imgur.com/kTYG6BM.png',
+                        url: 'https://i.imgur.com/HX79Nk6.png',
                     },
                     fields: [
                         { name: "Date", value: dates[0] + "\n" + dates[1] + "\n" + dates[2] + "\n" + dates[3] + "\n" + dates[4], inline: true },
                         { name: "Temperature", value: temps[0] + "\n" + temps[1] + "\n" + temps[2] + "\n" + temps[3] + "\n" + temps[4], inline: true }
                     ]
                 }
-            });
-
+            })
         })
 }
 
+function getDaily(data) {
+    var zipCode = args[0]
+    console.log(args[0])
+
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?zip=${zipCode},US&appid=002cc3ba839c181a79f9d6443cd15640`)
+        .then(function (response) { return response.json() })
+        .then(function (data) {
+            i = 0;
+            console.log(data);
+
+            tempF = Math.round(data.list[0].main.temp * 9 / 5 - 459.67);
+            tempC = Math.round(data.list[0].main.temp - 273.15);
+
+            var today = [Math.round(data.list[0].main.temp * 9 / 5 - 459.67)];
+            var temps = [Math.round(data.list[0].main.temp * 9 / 5 - 459.67)];
+            var dates = [data.list[0].dt_txt]
+
+            for (i = 8; i < 40; i = i + 8) {
+
+                var nextDay = Math.round(data.list[i].main.temp * 9 / 5 - 459.67);
+                temps.push(nextDay);
+                dates.push(data.list[i].dt_txt)
+            }
+            var fiveDay = client.channels.cache.get(channel).send({
+
+                embed: {
+                    color: 3449993,
+                    title: "5 Day Forecast:",
+                    thumbnail: {
+                        url: 'https://i.imgur.com/HX79Nk6.png',
+                    },
+                    fields: [
+                        { name: "Date", value: dates[0], inline: true },
+                        { name: "Temperature", value: temps[0], inline: true }
+                    ]
+                }
+            }).then(sentEmbed => {
+                sentEmbed.react("⬅️"),
+                    sentEmbed.react("➡️")
+                const filterLeft = (reaction, user) => reaction.emoji.name === '⬅️' && user.id != 734621174541320202;
+                const filterRight = (reaction, user) => reaction.emoji.name === '➡️' && user.id != 734621174541320202;
+                const collectorLeft = sentEmbed.createReactionCollector(filterLeft, { time: 15000 });
+                const collectorRight = sentEmbed.createReactionCollector(filterRight, { time: 15000 });
+                collectorRight.on('collect', r => {
+                    d++,
+                        dCheckHigh(d)
+
+                    sentEmbed.edit({
+                        embed: {
+                            color: 3449993,
+                            title: "5 Day Forecast",
+                            thumbnail: {
+                                url: 'https://i.imgur.com/HX79Nk6.png',
+                            },
+                            fields: [
+                                { name: "Date", value: dates[d], inline: true },
+                                { name: "Temperature", value: temps[d], inline: true }
+                            ]
+                        }
+                    })
+
+                })
+
+                collectorLeft.on('collect', r => {
+                    d = d - 1,
+                        dCheckLow(d),
+
+                        sentEmbed.edit({
+                            embed: {
+                                color: 3449993,
+                                title: "Next Day",
+                                thumbnail: {
+                                    url: 'https://i.imgur.com/HX79Nk6.png',
+                                },
+                                fields: [
+                                    { name: "Date", value: dates[d], inline: true },
+                                    { name: "Temperature", value: temps[d], inline: true }
+                                ]
+                            }
+                        })
+
+                });
+            })
+        })
+}
+function dCheckLow(d) {
+    if (d < 0) {
+        d = 4
+        return d;
+    }
+}
+function dCheckHigh(d) {
+    if (d > 4) {
+        d = 0
+        return d;
+    }
+}
+function removeReact() {
+
+
+}
 
 
 client.login(token);
